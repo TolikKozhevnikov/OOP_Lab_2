ed983f5f09e5f24097371bc021baef0fd1cdc786
TolikKozhevnikov <43010660+TolikKozhevnikov@users.noreply.github.com>
2020-01-10 22:36:27 +0300

    Update StudentDefualt.h

--- StudentDefualt.h
+++ StudentDefualt.h
@@ -7,6 +7,7 @@ class StudentDefualt : public Student
 public:
 	StudentDefualt(const string& Name1, const string& Name2, const string& Name3);
 	int GetRating() override;
+	int GetRatingAgain() override;
 };
 
 	
