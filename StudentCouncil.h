fba271de421568e13f5bd3c5283515e276355b40
TolikKozhevnikov <43010660+TolikKozhevnikov@users.noreply.github.com>
2020-01-10 22:35:46 +0300

    Update StudentCouncil.h

--- StudentCouncil.h
+++ StudentCouncil.h
@@ -6,5 +6,6 @@ class StudentCouncil : public Student
 public:
 	StudentCouncil(const string& Name1, const string& Name2, const string& Name3);
 	int GetRating() override;
+	int GetRatingAgain() override;
 };
 
